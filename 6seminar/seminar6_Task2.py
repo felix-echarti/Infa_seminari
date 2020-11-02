92476dc57b7a94a6d07b478fbaaed1cf1fc4f532
Felix <ekharti.fkh@phystech.edu>
2020-11-02 17:44:49 +0300

    Task 2 to seminar 6 (Vectors and operations with them). To be finished soon ... Directed by Robert B. Weide

---
+++ 6seminar/seminar6_Task2.py
@@ -0,0 +1,10 @@
+class Vector():
+    def __init__(self, x = 0, y = 0, z = 0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return print(str(self.x), str(self.y), str(self.z))
+
+
